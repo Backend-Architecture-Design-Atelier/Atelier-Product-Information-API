c935133781e89478772439ee84bafad7ebb98af7
Hansol Ji <ji1hansol@gmail.com>
2022-06-13 21:56:45 -0700

    clean up

--- server/models/index.js
+++ server/models/index.js
@@ -16,24 +16,19 @@ const getProductList = (page = 1, count = 5, callback) => {
 
 const getProductFeatures = (product_id, callback) => {
   const query = {
-    text: `SELECT
-    json_build_object(
-      'id', id,
-      'name', name,
-      'slogan', slogan,
-      'description', description,
-      'category', category,
-      'default_price', default_price,
-      'features', (
-        SELECT coalesce(json_agg(features), '[]'::json)
-        FROM (
-          SELECT feature, value
-          FROM features
-          WHERE product_id = p.id
-        ) as features
+    text: `SELECT json_build_object(
+      id,
+      name,
+      slogan,
+      description,
+      category,
+      default_price,
+      'features', (SELECT coalesce(json_agg(features), '[]'::json)
+        FROM (SELECT feature, value FROM features WHERE product_id = $1)
+        as features
       )
     ) as productfeatures
-    FROM product p WHERE id = $1`,
+    FROM product WHERE id = $1`,
     values: [product_id],
   };
   pool.query(query, (err, result) => {
@@ -49,30 +44,25 @@ const getProductStyles = (product_id, callback) => {
   const query = {
     text: `SELECT json_build_object(
       'product_id', id,
-      'results', (
-        SELECT json_agg(styles)
-        FROM (
-          SELECT
+      'results', (SELECT json_agg(styles)
+        FROM (SELECT
           id AS "style_id",
           name,
           original_price,
           sale_price,
           default_style AS "default?",
-          (
-            SELECT json_agg(photos) AS photos
-            FROM (
-              SELECT
+          (SELECT json_agg(photos) AS photos
+            FROM (SELECT
               thumbnail_url,
               url
               FROM photos WHERE styleId = styles.id
             ) AS photos
           ),
-          (
-            SELECT json_object_agg(
+          (SELECT json_object_agg(
               id,
               json_build_object(
-              'quantity', quantity,
-              'size', size
+                quantity,
+                size
               )
             ) FROM skus WHERE skus.styleId = styles.id
           ) AS skus
