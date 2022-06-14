86817abd90612d80c5a84a8caa64b7e35f1757ab
Hansol Ji <ji1hansol@gmail.com>
2022-06-13 19:40:47 -0700

    changed port agian for deployment

--- server/index.js
+++ server/index.js
@@ -8,6 +8,6 @@ app.use(express.json());
 // router
 app.use('/products', router);
 
-app.listen(3000, () => {
-  console.log(`Listening on port 3000`)
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`)
 });
