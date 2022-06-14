ae767b0328ddcb3d26e4d42f0df8ba4c6eeec741
Hansol Ji <ji1hansol@gmail.com>
2022-06-14 15:01:31 -0700

    added loader.io in routes

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,6 @@
 const controller = require('./controllers/index.js');
 const router = require('express').Router();
+require('dotenv').config();
 
 // connect controllers to their corresponding routes
 router.get('', controller.getProductList);
@@ -9,5 +10,9 @@ router.get('/:product_id', controller.getProductFeatures);
 router.get('/:product_id/styles', controller.getProductStyles);
 
 router.get('/:product_id/related', controller.getRelatedProducts);
+
+router.get(`/${process.env.LOADER}`, (req, res) => {
+  res.send(`${process.env.LOADER}`)
+})
 
 module.exports = router;
